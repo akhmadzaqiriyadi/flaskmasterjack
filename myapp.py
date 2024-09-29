6d0cff2b7891b064bafba4509c97e49c13231766
akhmadzaqiriyadi <akhmadzaqiriyadi@gmail.com>
2024-09-29 23:12:59 +0700

    Repairindex

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello Guys, I'm Jack"
 
 if __name__ == '__main__':
     app.run(debug=True)
