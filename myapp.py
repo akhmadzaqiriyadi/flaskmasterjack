77bb63df4bed4b4ee75fed72e853f11bc648fe57
akhmadzaqiriyadi <akhmadzaqiriyadi@gmail.com>
2024-10-01 17:17:01 +0700

    addtailwindcss&flowbite

--- myapp.py
+++ myapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Guys, I'm Jack"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
